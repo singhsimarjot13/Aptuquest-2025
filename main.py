f52dd73680c2b2a0af8ea356f931fdc6b9ad2092
Your Name <you@example.com>
2025-10-15 11:04:39 +0530

    added for deployment

--- main.py
+++ main.py
@@ -155,6 +155,8 @@ def google_login():
     try:
         if not google.authorized:
             return redirect(url_for("google.login"))
+        else:
+            flash("You are already authenticated")
 
         resp = google.get("/oauth2/v2/userinfo")
         if not resp.ok:
